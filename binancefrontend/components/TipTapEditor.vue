ca2ae6395738380033e60ec21e9724ab518bbbcb
alima420 <165353508+alima420@users.noreply.github.com>
2025-01-12 01:04:23 +0100

    tiptapeditor bugs

--- binancefrontend/components/TipTapEditor.vue
+++ binancefrontend/components/TipTapEditor.vue
@@ -257,10 +257,6 @@ const executeEditorCommand = (button) => {
   border: 1px solid #e0e0e0;
 }
 
-.toolbar-section button.is-active {
-  
-}
-
 .toolbar-section button:disabled {
   opacity: 0.5;
   cursor: not-allowed;
